f580bc7ce1b5caee58faf85b35c0643de92a5a7f
Beardevelope <mck84835182@gmail.com>
2023-10-25 19:42:53 +0900

    delete mudule

--- modules/moveList.js
+++ modules/moveList.js
@@ -1,11 +1,39 @@
 // 메인페이지 영화 리스트 및 검색기능
-import { getTodayMovies } from "./data.js";
 
 const getMovies = async () => {
-  const result = await getTodayMovies();
+    const result = await getAllMovies();
 
-  console.log(result);
-};
+    console.log(result);
 
+    let cardList = document.querySelector('.movieBox');
+    cardList.innerHTML = null;
+
+    let titleList = document.querySelector('result.')
+
+    if (title !== undefined) {
+        result = result.filter((obj) => {
+            if (obj.title.search(title) !== -1) {
+                return true;
+            }
+        })
+    }
+
+    let allMovies = document.querySelector(".movieBox")
+    movieObj.forEach(infoMovie => {
+        let temp_html =
+            `
+    <div class="col">
+    <div class="card">
+        <img src=""https://image.tmdb.org/t/p/w500/${infoMovie.poster_path}"" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${infoMovie.title}</h5>
+            <p class="card-text">${infoMovie.overview}</p>
+        </div>
+    </div>
+    </div>
+`
+        allMovies.insertAdjacentHTML('beforeend', temp_html);
+    });
+}
 getMovies();
 
